20f16e4c000ec225970bb40f728839c98708d9fe
Jefferson0k <jefersoncovenas7@gmail.com>
2024-09-30 08:19:23 -0500

    Correciones

--- taskapi/urls.py
+++ taskapi/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lsp/', include('lsp.urls')),
+    path('tasks/', include('tasks.urls')),
 ]
